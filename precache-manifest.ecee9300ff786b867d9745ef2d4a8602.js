ee522aa5e5c7cccb4f4b792dfea2449b21e968b1
YubaNeupane <neopaneyb2@gmail.com>
2020-06-08 20:13:57 -0400

    Updates

---
+++ precache-manifest.ecee9300ff786b867d9745ef2d4a8602.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c34397c4561348837687395df0d8c156",
+    "url": "/Covid-19-Tracker/index.html"
+  },
+  {
+    "revision": "c107d8fdd4cc708209f5",
+    "url": "/Covid-19-Tracker/static/css/main.8e328160.chunk.css"
+  },
+  {
+    "revision": "99d6e6d47cd6bba55bd3",
+    "url": "/Covid-19-Tracker/static/js/2.8634b111.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/Covid-19-Tracker/static/js/2.8634b111.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c107d8fdd4cc708209f5",
+    "url": "/Covid-19-Tracker/static/js/main.cc20233a.chunk.js"
+  },
+  {
+    "revision": "4deed42139d1aadd1ede",
+    "url": "/Covid-19-Tracker/static/js/runtime-main.59ac251f.js"
+  }
+]);
